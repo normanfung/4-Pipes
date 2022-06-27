030e51badbe1fce2350c70fcc0e938d4ebf9f343
normanfung <normanfung96@gmail.com>
2022-06-27 15:44:43 +0800

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '4-Pipes';
+  name: string;
+  paymentDate: Date;
+  paymentAmount: number;
+  miles: number;
+
+  onNameChange(payeeName: string) {
+    this.name = payeeName;
+  }
+
+  onDateChange(date: string) {
+    this.paymentDate = new Date(date);
+  }
+
+  onAmountChange(amount: number) {
+    this.paymentAmount = amount;
+  }
+
+  onMilesChange(miles: number) {
+    this.miles = miles;
+  }
 }
